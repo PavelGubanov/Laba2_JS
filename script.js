576a1abfca50ff945870b1e68d59045ed5837b83
PavelGubanov_HP <pavelgubanov2001@mail.ru>
2020-06-09 00:46:00 +0300

    Добавил функцию добавления ячейки и строки в супертаблицу

--- script.js
+++ script.js
@@ -38,3 +38,41 @@ function sortCarTable (index, dataType, tableIsSorted) {
     }
     carTable.appendChild(tbody);
 }
+
+function addCellInCarTable (row, value) {
+    row.insertCell(-1).appendChild(document.createTextNode(value));
+}
+
+function addRowInCarTable () {
+    let model = document.getElementById('inputModel');
+    let enginePower = document.getElementById('inputEnginePower');
+    let trunkVolume = document.getElementById('inputTrunkVolume');
+    let clearance = document.getElementById('inputClearance');
+    let cost = document.getElementById('inputCost');
+
+    if ((model.value != '') && (parseInt(enginePower.value)) &&
+        (parseInt(trunkVolume.value)) && (parseInt(clearance.value)) &&
+        (parseInt(cost.value)))
+    {
+        let row = carTable.getElementsByTagName('tbody')[0].insertRow(-1);
+        addCellInCarTable(row, model.value);
+        addCellInCarTable(row, enginePower.value);
+        addCellInCarTable(row, trunkVolume.value);
+        addCellInCarTable(row, clearance.value);
+        addCellInCarTable(row, cost.value);
+
+        let deleteButton = document.createElement( "button");
+        deleteButton.innerHTML = '<img src = "TrashCan.png" alt = "Удалить">';
+        row.insertCell(-1).appendChild(deleteButton);
+
+        model.value = '';
+        enginePower.value = '';
+        trunkVolume.value = '';
+        clearance.value = '';
+        cost.value = '';
+    }
+    else
+    {
+        alert('Ошибка! Необходимо все поля заполнить корректными значениями!');
+    }
+}
