33d7cbc57889ea8e7135207bc4c9f6a63af91d3e
PavelGubanov_HP <pavelgubanov2001@mail.ru>
2020-06-09 00:42:41 +0300

    Добавил сортировку супертаблицы

--- script.js
+++ script.js
@@ -5,3 +5,36 @@ let idForm = 'AddNewCarInTable';
 
 let carTable = document.getElementById(idTable);
 let form = document.getElementById(idForm);
+
+let selectedColumn = -1;
+
+function sortCarTable (index, dataType, tableIsSorted) {
+    const tbody = carTable.querySelector('tbody');
+    function compareRows (firstRow, secondRow){
+        const dataFirstRow = firstRow.cells[index].innerHTML;
+        const dataSecondRow = secondRow.cells[index].innerHTML;
+        switch (dataType) {
+            case 'integer': {
+                return dataFirstRow - dataSecondRow;
+            }
+            case 'text': {
+                if (dataFirstRow < dataSecondRow)
+                    return -1;
+                if (dataFirstRow > dataSecondRow)
+                    return 1;
+                return 0;
+            }
+        }
+    }
+
+    let rows = [].slice.call(tbody.rows);
+    rows.sort(compareRows);
+    if (tableIsSorted)
+        rows.reverse();
+
+    carTable.removeChild(tbody);
+    for (let i = 0; i < rows.length; i++){
+        tbody.appendChild(rows[i]);
+    }
+    carTable.appendChild(tbody);
+}
